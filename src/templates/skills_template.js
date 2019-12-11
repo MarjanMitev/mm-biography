cbbb68a1b5fe488eada522399faca206ff55a985
Marjan Mitev <sermarjan@gmail.com>
2019-12-11 22:14:21 +0100

    Added skills page tempalte and component

--- src/templates/skills_template.js
+++ src/templates/skills_template.js
@@ -8,7 +8,23 @@ export default class SkillsTemplate extends React.Component {
         return (
             <Layout {...this.props}>
               <article className="post page post-full">
-                <Skills {...this.props} />
+                <header className="post-header">
+                  <h1 className="post-title underline">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
+                </header>
+                {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
+                <div className="post-subtitle">
+                  {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle'))}
+                </div>
+                }
+                {_.get(this.props, 'pageContext.frontmatter.img_path') && 
+                <div className="post-thumbnail">
+                  <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.img_path'))} alt={_.get(this.props, 'pageContext.frontmatter.title')} />
+                </div>
+                }
+                <div className="post-content">
+                  {htmlToReact(_.get(this.props, 'pageContext.html'))}
+                  <Skills {...this.props}/>
+                </div>
               </article>
             </Layout>
         );

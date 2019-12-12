1685f6c8b43a92ba1829cf0ee79ac4de4d29d8e3
Marjan Mitev <marjan.mitev@massive.co>
2019-12-12 10:58:00 +0100

    added skills scss and modified skill.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,14 +4,15 @@ import _ from 'lodash';
 export default class Skills extends React.Component {
     render() {
         return (
-            <div className="social-links">
-                <h2>Skills</h2>
-                {_.map(_.get(this.props, 'pageContext.site.data.skills.core_skills'), (skill, id) => (
-                skill && 
-                    <div key={id}>
-                        {_.get(skill, 'title')}
-                    </div>
-                ))}
+            <div className="skills-wrapper">
+                <ul className="skills-list">
+                    {_.map(_.get(this.props, 'pageContext.site.data.skills.core_skills'), (skill, id) => (
+                    skill && 
+                        <li className="skill-item" key={id}>
+                            <span className="skill-progress">{_.get(skill, 'title')}</span>
+                        </li>
+                    ))}
+                </ul>
           </div>
         );
     }
